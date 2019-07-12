81688098aeb2627cd14f688cb767781a5bd2502b
Alexey Zayats <cheburah2018@gmail.com>
2019-07-12 15:19:51 +0300

    Logic test

--- src/main/java/com/exadelinternship/carpool/controllers/UserController.java
+++ src/main/java/com/exadelinternship/carpool/controllers/UserController.java
@@ -2,26 +2,33 @@ package com.exadelinternship.carpool.controllers;
 
 
 import com.exadelinternship.carpool.entity.User;
+import com.exadelinternship.carpool.entity.dto.UserProfileDTO;
+import com.exadelinternship.carpool.impl.UserServiceImpl;
 import com.exadelinternship.carpool.repository.UserRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.List;
+
 
 @RestController
-@RequestMapping("/api/v1")
 public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping("/users")
-    public List<User> getAllUsers(){
-        return userRepository.findAll();
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @GetMapping("/main/{userId}")
+    @ResponseBody
+    public UserProfileDTO getUser(@PathVariable long userId){
+        return convertToDto(userRepository.findById(userId));
     }
 
-    @PostMapping("/users")
-    public User createUser(@Valid @RequestBody User user){
-        return userRepository.save(user);
+    private UserProfileDTO convertToDto(User user) {
+        UserProfileDTO userDto = modelMapper.map(user, UserProfileDTO.class);
+        return userDto;
     }
+
 }
--- src/main/java/com/exadelinternship/carpool/repository/UserRepository.java
+++ src/main/java/com/exadelinternship/carpool/repository/UserRepository.java
@@ -1,10 +1,14 @@
 package com.exadelinternship.carpool.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.exadelinternship.carpool.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("select u from User u where u.id= :id")
+    User findById(@Param("id")long id);
 }
